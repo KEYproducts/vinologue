1607aad0fd54d76236221bb6ebc1b61b6659da6b
KEYproducts <keiko.yamamoto51@gmail.com>
2023-06-16 19:02:15 +0900

    アソシエーションとバリデーションの設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ApplicationRecord
+  belongs_to :user, optional: true
+  
   enum sex: { male: 1, female: 2, not_answer: 3 }
+
+  validates :sex,           presence: true
+  validates :age,           numericality: { greater_than_or_equal_to: 20 }
+  validates :prefecture_id, numericality: { other_than: 1 }
 end

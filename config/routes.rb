c1a61ad3f217c44034123d3f695e222649425c5e
KEYproducts <keiko.yamamoto51@gmail.com>
2023-06-17 21:51:09 +0900

    winesコントローラーの作成、ルーティングの設定
    コントローラー
    ついでにwines#newのビューファイルを作成
    
    ルーティング
    ①wines#new, createを設定
    ②マイページ用にユーザーにネストしたwines#user_winesを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
     get 'profiles', to: 'users/registrations#new_profile'
     post 'profiles', to: 'users/registrations#create_profile'
   end
+
+  resources :wines, only: [:new, :create]
+
+  resources :users, only: :show do
+    get 'wines', to: 'wines#user_wines'
+  end
+
   root to: 'users#index'
 end

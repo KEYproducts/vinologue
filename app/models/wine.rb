7170964f3caf443ec609ba4181592dc0d70d1b37
KEYproducts <keiko.yamamoto51@gmail.com>
2023-06-18 19:04:21 +0900

    アソシエーション追記

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,10 @@
 class Wine < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :type
+  belongs_to :country
+  belongs_to :variety
+  belongs_to :star
 end

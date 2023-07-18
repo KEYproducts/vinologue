3c59dbc218215e4b01773e9e1cc249595ee8552d
KEYproducts <keiko.yamamoto51@gmail.com>
2023-07-18 12:07:51 +0900

    rubocopによる整形

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,5 +8,5 @@ class Profile < ApplicationRecord
     validates :age, numericality: { only_integer: true, message: 'は半角数字で入力してください' },
                     inclusion: { in: 20..150, message: 'が20歳未満の方は登録できません' }
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を入力してください' }
 end
